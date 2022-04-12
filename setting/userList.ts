d7c150f0ac9471e27d6a1123754f9fd5268864ef
Fred <fengyiyi@users.noreply.github.com>
2022-04-12 15:31:56 +0800

    Update userList.ts

--- setting/userList.ts
+++ setting/userList.ts
@@ -1,9 +1,8 @@
 const userList: userListType = {
-    users: ["fengyiyi@outlook.com", "fengyiyi_outlook.com#EXT#@fengyiyioutlook.onmicrosoft.com", "f_yiyi@hotmail.com"],
+    users: ["admin@fengyiyi.onmicrosoft.com", "AdeleV@fengyiyi.onmicrosoft.com"],
     nickname: {
-        "fengyiyi@outlook.com": '🤝 分享功能测试',
-        "fengyiyi_outlook.com#EXT#@fengyiyioutlook.onmicrosoft.com": '🌟 ☁OneSS † 开发测试用 †',
-        "f_yiyi@hotmail.com": '个人文件',
+        "admin@fengyiyi.onmicrosoft.com": '个人文件',
+        "AdeleV@fengyiyi.onmicrosoft.com": '🌟 ☁OneSS † 开发测试用 †',
     }
 }
 export default userList;
